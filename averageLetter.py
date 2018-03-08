69674128db966c9e8406aa30b2a8b3bbb359c686
Tim <mallardbro@hotmail.co.uk>
2018-03-08 23:54:05 +0000

    Initial start on get_average function. More error checks required?

---
+++ averageLetter.py
@@ -0,0 +1,12 @@
+def get_average(str):
+    alphabet = " abcdefghijklmnopqrstuvwxyz"
+    s = 0
+    l = 0
+    for c in str.lower():
+        if c in alphabet:
+            s += alphabet.index(c)
+            l += 1
+    return float(s)/l
+
+
+print(get_average("Mallard"))
