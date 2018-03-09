0e4bdbf114f48926540cfa0e03547e53d29f44ed
Tim <mallardbro@hotmail.co.uk>
2018-03-09 00:11:22 +0000

    Added word_cost function, and made Raynor feel valued.

--- averageLetter.py
+++ averageLetter.py
@@ -1,13 +1,28 @@
-def get_average(str):
+def get_average(_str):
     alphabet = " abcdefghijklmnopqrstuvwxyz"
     s = 0
     l = 0
-    for c in str.lower():
+    for c in _str.lower():
         if c in alphabet:
             s += alphabet.index(c)
             l += 1
     return float(s)/l
 
 
-print(get_average("Mallard"))
+def word_cost(_str, vowel, cons):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    vowels = "aeiou"
+    out = 0
+    for c in _str.lower():
+        if c in alphabet:
+            if c in vowels:
+                out += vowel
+            else:
+                out += cons
+    return out
+
+
 print(get_average("Raynor"))
+print(word_cost("Mallard",150, 25))
+
+
